dbb22b7f87c91b5477fc51edaf6bc99ecce1b68c
Craig Winter <cajwinter@gmail.com>
2015-07-05 09:39:50 +1000

    new js file

--- bcc_nav/static/js/estbay_eamtay.js
+++ bcc_nav/static/js/estbay_eamtay.js
@@ -1,64 +1,190 @@
-function initialize() {
-    
-    
-    function addMarker(theposition, themap, theicon, thetitle, theinfo) {
-        
-    if (theicon == 'default') {
-        
-        var theicon = '../static/img/icon1.png';
-    }
-        
-    var contentString = '<div id="content">' + theinfo + '</div>';
-        
-    var infowindow = new google.maps.InfoWindow({
-          content: contentString
-      });
-        
-    var marker = new google.maps.Marker({
-          position: theposition,
-          map: themap,
-          icon: theicon,
-          title: thetitle
-      });
-        
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(themap,marker);
-      });
-        
-    }
-    
-        var mapOptions = {
-          center: { lat: -27.480329, lng: 153.0245508},
-          zoom: 13,
-            mapTypeId: google.maps.MapTypeId.TERRAIN
-        };
-    
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+  for (var i = 0; i < allmarkers2.length; i++) {
+    allmarkers2[i].setMap(map);
+  }
+}
 
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setAllMap(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  allmarkers = [];
+  allmarkers2 = [];
+}
+
+function buttonCheck() {
+    if (ferryOn == true) {
+        addFerries()
+    }
+
+    if (libraryOn == true) {
+        addlibraries()
+    }
+
+    if (wifiOn == true) {
+        addWiFi()
+    } 
+
+    if (busOn == true) {
+        addBuses()
+    } 
+
+    if (bikeOn == true) {
+        addBikes()
+    } 
+}
+
+function ferrySwap() {
+    if (ferryOn == false) {
+        ferryOn = true;
+    } else {
+        ferryOn = false;   
+    }
+    rePaint()
+}
+
+function librarySwap() {
+    if (libraryOn == false) {
+        libraryOn = true;
+    } else {
+        libraryOn = false;   
+    }
+    rePaint()
+}
+
+function wifiSwap() {
+    if (wifiOn == false) {
+        wifiOn = true;
+    } else {
+        wifiOn = false;   
+    }
+    rePaint()
+}
+
+function busSwap() {
+    if (busOn == false) {
+        busOn = true;
+    } else {
+        busOn = false;   
+    }
+    rePaint()
+}
+
+function bikeSwap() {
+    if (bikeOn == false) {
+        bikeOn = true;
+    } else {
+        bikeOn = false;   
+    }
+    rePaint()
+}
+
+function addBikes() {
     /* bike racks */
-        /*for (i = 0; i < bikerackdata.length; i++) { 
-            addMarker(new google.maps.LatLng(bikerackdata[i][0],bikerackdata[i][1]), map, bikerackdata[i][2], bikerackdata[i][3], bikerackdata[i][4])
-        }*/
-    
-    /* bus stops */
-        /*for (i = 0; i < busstopdata.length; i++) { 
-            addMarker(new google.maps.LatLng(busstopdata[i][0],busstopdata[i][1]), map, busstopdata[i][2], busstopdata[i][3], busstopdata[i][4])
-        }*/
-    
-    /* wi fi */
-    /*for (i = 0; i < wifidata.length; i++) { 
-            addMarker(new google.maps.LatLng(wifidata[i][0],wifidata[i][1]), map, wifidata[i][2], wifidata[i][3], wifidata[i][4])
-        }*/
-    
-    /*for (i = 0; i < librariesdata.length; i++) { 
-            addMarker(new google.maps.LatLng(librariesdata[i][0],librariesdata[i][1]), map, librariesdata[i][2], librariesdata[i][3], librariesdata[i][4])
-        }*/
-    
-    for (i = 0; i < ferriesdata.length; i++) { 
-            addMarker(new google.maps.LatLng(ferriesdata[i][0],ferriesdata[i][1]), map, ferriesdata[i][2], ferriesdata[i][3], ferriesdata[i][4])
+        for (i = 0; i < bikerackdata.length; i++) { 
+            allmarkers.push(bikerackdata[i])
         }
+}
+
+function addBuses() {        
+        /* bus stops */
+        for (i = 0; i < busstopdata.length; i++) { 
+            allmarkers.push(busstopdata[i])
+        } 
+}
+
+function addWiFi() {
+         /* wi fi */
+        for (i = 0; i < wifidata.length; i++) { 
+            allmarkers.push(wifidata[i])
+        }
+}
+
+function addlibraries() {
+        for (i = 0; i < librariesdata.length; i++) { 
+        allmarkers.push(librariesdata[i])
+        }
+}
+
+function addFerries() {
+        for (i = 0; i < ferriesdata.length; i++) { 
+           allmarkers.push(ferriesdata[i])
+        }
+}
+
+function renderMarkers() {
+    for (i = 0; i < allmarkers.length; i++) { 
+        addMarker(new google.maps.LatLng(allmarkers[i][0],allmarkers[i][1]), allmarkers[i][2], allmarkers[i][3], allmarkers[i][4])
+    }
+
+}
+
+function addMarker(theposition, theicon, thetitle, theinfo) {
+
+if (theicon == 'default') {
+
+    var theicon = '../static/img/icon1.png';
+}
+
+var contentString = '<div id="content">' + theinfo + '</div>';
+
+var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+var marker = new google.maps.Marker({
+      position: theposition,
+      map: map,
+      icon: theicon,
+      title: thetitle
+  });
+
+google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
+  });
+
+    allmarkers2.push(marker);
+
+}
+
+function rePaint() {
+    /* adds all the markers based on what buttons are checked then renders them */
+    deleteMarkers()
+    buttonCheck();
+    renderMarkers();
     
 }
 
-      google.maps.event.addDomListener(window, 'load', initialize);
+var map;
+var allmarkers = [];
+var allmarkers2 = [];
+var bikeOn = false;
+var busOn = false;
+var ferryOn = false;
+var libraryOn = false;
+var wifiOn = false;
+
+function initialize() {
+ 
+        var mapOptions = {
+          center: { lat: -27.480329, lng: 153.0245508},
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+    
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+    
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
