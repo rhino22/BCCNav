5f57523ab2d6f967461317649592485860b356cd
Craig Winter <cajwinter@gmail.com>
2015-07-04 19:44:37 +1000

    the data and icons

--- bcc_nav/static/js/estbay_eamtay.js
+++ bcc_nav/static/js/estbay_eamtay.js
@@ -29,14 +29,15 @@ function initialize() {
     
         var mapOptions = {
           center: { lat: -27.480329, lng: 153.0245508},
-          zoom: 13
+          zoom: 13,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
         };
     
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
 
     /* bike racks */
-       /* for (i = 0; i < bikerackdata.length; i++) { 
+        /*for (i = 0; i < bikerackdata.length; i++) { 
             addMarker(new google.maps.LatLng(bikerackdata[i][0],bikerackdata[i][1]), map, bikerackdata[i][2], bikerackdata[i][3], bikerackdata[i][4])
         }*/
     
@@ -50,8 +51,12 @@ function initialize() {
             addMarker(new google.maps.LatLng(wifidata[i][0],wifidata[i][1]), map, wifidata[i][2], wifidata[i][3], wifidata[i][4])
         }*/
     
-    for (i = 0; i < librariesdata.length; i++) { 
+    /*for (i = 0; i < librariesdata.length; i++) { 
             addMarker(new google.maps.LatLng(librariesdata[i][0],librariesdata[i][1]), map, librariesdata[i][2], librariesdata[i][3], librariesdata[i][4])
+        }*/
+    
+    for (i = 0; i < ferriesdata.length; i++) { 
+            addMarker(new google.maps.LatLng(ferriesdata[i][0],ferriesdata[i][1]), map, ferriesdata[i][2], ferriesdata[i][3], ferriesdata[i][4])
         }
     
 }
